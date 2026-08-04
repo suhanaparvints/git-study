3520dd5a2bea99d062c864799bac3453f40d9100
suhanaparvints <suhanaparvints@gmail.com>
2026-08-04 19:44:39 +0530

    stash applied

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function showMessage(){
     alert("Welcome to my Git Practice Project!");
 
 }
+function showMessage(){
+<<<<<<< Updated upstream
+
+    alert("Welcome to my Git Practice Project!");
+
+=======
+    alert("hello")
+>>>>>>> Stashed changes
+}
