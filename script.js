70db28c33f5c63f513424f2f251501b348d3a6b7
suhanaparvints <suhanaparvints@gmail.com>
2026-08-04 19:47:02 +0530

    updated 2 files

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 function showMessage(){
 
     alert("Welcome to my Git Practice Project!");
-
-}
-function showMessage(){
-<<<<<<< Updated upstream
-
-    alert("Welcome to my Git Practice Project!");
-
-=======
-    alert("hello")
->>>>>>> Stashed changes
 }
